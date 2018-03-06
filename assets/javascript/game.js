5d2349ca8992cc520f8f5ce026460345baeae81a
Kyle Harrer <harrer.kyle@gmail.com>
2018-03-06 18:16:15 -0500

    objects created

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+// Declare the character objects
+var obiWan {
+    name: "Obi Wan Kenobi" , 
+    attack : 8,
+    HP : 120,
+    counterAttack : 8,
+    };
+
+var lukeSky {
+    name: "Luke Skywalker" ,
+    attack : 5,
+    HP : 100,
+    counterAttack : 5,
+    };
+
+var darthSid {
+    name: "Darth Sidious" ,
+    attack : 20,
+    HP : 150,
+    counterAttack : 20,
+    };
+
+var darthMaul {
+    name : "Darth Maul" ,
+    attack : 25,
+    HP : 180,
+    counterAttack : 25,
+    };
+
+// Other global variables
+var dead = false;
+var wins = 0;
+
+
+$(document).ready(function() {
+    
+})
