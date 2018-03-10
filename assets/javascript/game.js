92a8360197f5cc6f25728f60e4fe5c717663486d
Kyle Harrer <harrer.kyle@gmail.com>
2018-03-10 11:06:25 -0500

    re-work character objects into one object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,60 @@
 // Declare the character objects
-var obiWan = {
-    name: "Obi Wan Kenobi",
-    attack: 8,
-    HP: 120,
-    counterAttack: 8,
+
+// var obiWan = {
+//     name: "Obi Wan Kenobi",
+//     attack: 8,
+//     HP: 120,
+//     counterAttack: 8,
+// };
+
+// var lukeSky = {
+//     name: "Luke Skywalker",
+//     attack: 5,
+//     HP: 100,
+//     counterAttack: 5,
+// };
+
+// var darthSid = {
+//     name: "Darth Sidious",
+//     attack: 20,
+//     HP: 150,
+//     counterAttack: 20,
+// };
+
+// var darthMaul = {
+//     name: "Darth Maul",
+//     attack: 25,
+//     HP: 180,
+//     counterAttack: 25,
+// };
+
+var charList = {
+    obiWan: {
+        name: "Obi Wan Kenobi",
+        attack: 8,
+        HP: 120,
+        counterAttack: 8,
+    },
+    lukeSky: {
+        name: "Luke Skywalker",
+        attack: 5,
+        HP: 100,
+        counterAttack: 5,
+    },
+    darthSid: {
+        name: "Darth Sidious",
+        attack: 20,
+        HP: 150,
+        counterAttack: 20,
+    },
+    darthMaul: {
+        name: "Darth Maul",
+        attack: 25,
+        HP: 180,
+        counterAttack: 25,
+    }
 };
 
-var lukeSky = {
-    name: "Luke Skywalker",
-    attack: 5,
-    HP: 100,
-    counterAttack: 5,
-};
-
-var darthSid = {
-    name: "Darth Sidious",
-    attack: 20,
-    HP: 150,
-    counterAttack: 20,
-};
-
-var darthMaul = {
-    name: "Darth Maul",
-    attack: 25,
-    HP: 180,
-    counterAttack: 25,
-};
 
 // Define variables for selected fighter and defender, allowing program to keep original values for reset
 var fighter = {};
@@ -62,14 +91,31 @@ function reset() {
 
 // Main body of game
 $(document).ready(function () {
-    $('.character').click(function () {
+
+    //select main character and move the others to enemies
+    $('.characters').click('character', function (event) {
+        var id = event.target.id;
+        console.log('the id is: ' + id);
         if (fighterSelect === false) {
-            createFighter(this);
+            createFighter(charList[id]);
             fighterSelect = true;
             $(this).removeClass('character').addClass('your-character');
             $('.character').removeClass('character').addClass('enemy');
             $('.enemies').prepend($('.enemy'));
-            $('.characters').html($(this))
+            $('.characters').html($(this));
         }
-    })
-})
+    });
+
+    // Select one of the enemies available to attack and moves them to defender
+    $('.enemies').click('.enemy',function (event) {
+        console.log("test")
+        var id = event.target.id;
+        if (defenderSelect === false) {
+            createDefender(this);
+            defenderSelect = true;
+            $(this).removeClass('enemy').addClass('defender');
+            $('.defenders').prepend($('.defender'));
+            $('')
+        }
+    });
+});
